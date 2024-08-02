a58c0adc63518e7878b6196c031f9d29387812ce
Mariano <marianoberton@gmail.com>
2024-08-02 15:57:26 -0300

    Update database configuration and other changes

--- config/admin.js
+++ config/admin.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', '6v67Jo0zddzrEsU4gaW6DA=='),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'VHQqiRheSJqApKJa4ctelw=='),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', '6k9Soyv1YrvRnknZkTking=='),
     },
   },
   flags: {
--- config/server.js
+++ config/server.js
@@ -1,8 +1,13 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '127.0.0.1'),
+  host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', [
+      'GPUNCKqg79YAt+MDPNFW/A==',
+      'fiDKbfe4tQNB2P9au6aM+w==',
+      'nmRsqaDJsvw3LUGt4oYw5Q==',
+      'OlKhDEDx9sfA+Y43dO2T+w=='
+    ]),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
