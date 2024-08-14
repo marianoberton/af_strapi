6fee65672efd3d962c170eb55b56fa2a86ee71ca
Mariano <marianoberton@gmail.com>
2024-08-14 15:33:03 -0300

    Actualizacion de persistencia

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,20 @@
-module.exports = () => ({});
+// config/plugins.js
+module.exports = ({ env }) => ({
+    upload: {
+      config: {
+        provider: 'local',
+        providerOptions: {
+          sizeLimit: 1000000000, // 1GB
+        },
+        breakpoints: {
+          large: 1000,
+          medium: 750,
+          small: 500,
+          thumbnail: 64,
+        },
+      },
+      // Ajusta la ruta aquí al directorio de tu carpeta uploads en tu disco persistente
+      path: 'src/database/uploads',
+    },
+  });
+  
