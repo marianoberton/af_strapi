6efbb560f32b05e280c981491bb596304ec4c25f
Mariano <marianoberton@gmail.com>
2024-08-15 11:51:03 -0300

    Image handling

--- config/middlewares.js
+++ config/middlewares.js
@@ -10,3 +10,28 @@ module.exports = [
   'strapi::favicon',
   'strapi::public',
 ];
+module.exports = [
+  'strapi::logger',
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", "https://res.cloudinary.com/dedryjsvs/"],
+          "media-src": ["'self'", "data:", "blob:"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
--- config/plugins.js
+++ config/plugins.js
@@ -1,19 +1,28 @@
 // config/plugins.js
 module.exports = ({ env }) => ({
-    upload: {
-      config: {
-        provider: 'local',
-        providerOptions: {
-          sizeLimit: 1000000000, // 1GB
-        },
-        breakpoints: {
-          large: 1000,
-          medium: 750,
-          small: 500,
-          thumbnail: 64,
-        },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        // Configuración para Cloudinary
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+        // Configuración para local
+        sizeLimit: 1000000000, // 1GB para el proveedor local
       },
-      path: 'src/database', // Configura el directorio padre persistente
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+      breakpoints: {
+        large: 1000,
+        medium: 750,
+        small: 500,
+        thumbnail: 64,
+      },
     },
-  });
-  
+    path: 'src/database', // Configura el directorio padre persistente
+  },
+});
