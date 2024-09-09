7e27efdedb86b74a640ff506d7a4d48767c4cde9
Mariano <marianoberton@gmail.com>
2024-09-09 15:57:34 -0300

    Eliminé claves API de los commits

--- config/plugins.js
+++ config/plugins.js
@@ -1,39 +1,43 @@
-module.exports = ({ env }) => ({
-  upload: {
-    config: {
-      provider: "cloudinary",
-      providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
-        sizeLimit: 1000000000, // 1GB
+module.exports = ({ env }) => {
+  console.log('SendGrid API Key:', env('SENDGRID_API_KEY')); // Mostrar la clave API de SendGrid
+
+  return {
+    upload: {
+      config: {
+        provider: "cloudinary",
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key: env('CLOUDINARY_KEY'),
+          api_secret: env('CLOUDINARY_SECRET'),
+          sizeLimit: 1000000000, // 1GB
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
+        },
+        breakpoints: {
+          large: 1000,
+          medium: 750,
+          small: 500,
+          thumbnail: 64,
+        },
       },
-      actionOptions: {
-        upload: {},
-        uploadStream: {},
-        delete: {},
-      },
-      breakpoints: {
-        large: 1000,
-        medium: 750,
-        small: 500,
-        thumbnail: 64,
+      path: 'src/database', // Configura el directorio padre persistente
+    },
+
+    // Configuración de SendGrid
+    email: {
+      config: {
+        provider: 'sendgrid',
+        providerOptions: {
+          apiKey: env('SENDGRID_API_KEY'),
+        },
+        settings: {
+          defaultFrom: 'marianoberton@hotmail.com',
+          defaultReplyTo: 'consultasforchieri@gmail.com',
+        },
       },
     },
-    path: 'src/database', // Configura el directorio padre persistente
-  },
-  
-  // Configuración de SendGrid
-  email: {
-    config: {
-      provider: 'sendgrid',
-      providerOptions: {
-        apiKey: env('SENDGRID_API_KEY'),
-      },
-      settings: {
-        defaultFrom: 'marianoberton@hotmail.com',
-        defaultReplyTo: 'marianoberton@hotmail.com',
-      },
-    },
-  },
-});
+  };
+};
